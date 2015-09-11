8c58466a49bcf7ae31b6bd703a25f9f7b9f90029
czxichen <czxichen@163.com>
2015-09-11 09:39:22 +0800

    20150911

---
+++ 分割空白符号/split.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	a := "cz 			 	xi	  chen		"
+	fmt.Println(split(a))
+}
+func split(str string) []string {
+	var l []string
+	list := strings.Split(str, " ")
+	for _, v := range list {
+		if len(v) == 0 {
+			continue
+		}
+		if strings.Contains(v, "	") {
+
+			list := strings.Split(v, "	")
+			for _, v := range list {
+				if len(v) == 0 {
+					continue
+				}
+				l = append(l, v)
+			}
+			continue
+		}
+		l = append(l, v)
+	}
+	return l
+}
